cf3b45d3f44faba02771492927db76b24db21f56
Acsa Lu <acsa0210@gmail.com>
2013-11-02 12:07:24 +0800

    Parse graph

--- graph.py
+++ graph.py
@@ -1,33 +1,32 @@
-class Graph:
-    def __init__(self):
-        self.nodes = []
-        self.edges = []
+import ParsePy
+from igraph import *
 
-    def createNode(self, lat, lng):
-        self.nodes.append(Node(lat, lng))
+def initParse():
+    ParsePy.APPLICATION_ID = "9viWGl0x5YuJqhwg8dbYG16snlA2WG5X26JJrI7d"
+    ParsePy.MASTER_KEY = "WWEZS3H9LCadSHLhwBPIbVk3nDEjeBAhEMGWDIJr"
+    
+def fetchGraph():
+    
+    g = Graph()
 
-    def createEdge(self, node1, node2):
-        e = Edge(node1, node2)
-        node1.edges.append(e)
-        node2.edges.append(e)
-        self.edges.append(e)
+    query = ParsePy.ParseQuery("Node")
+    nodes = query.fetch()
+    query = ParsePy.ParseQuery("Edge")
+    edges = query.fetch()
+
+    
+    g.add_vertices(len(nodes))
+
+    for node in nodes:
+        v = g.vs[nodes.index(node)]
+        v['lat'] = node.lat
+        v['lng'] = node.lng
+        print("node (%.6f, %.6f)" % (node.lat, node.lng))
+
+    for e in edges:
+        v1 = g.vs[nodes.index(e.node1)]
+        v2 = g.vs[nodes.index(e.node2)]
 
 
 
-class Node:
-    def __init__(self, lat=0, lng=0):
-        self.attributes = []
-        self.edges = []
-        self.lat = lat
-        self.lng = lng
-
-
-class Edge:
-    def __init__(self, length=0):
-        self.length = length
-        self.node1 = node1
-        self.node2 = node2
-
-    def is_connected(self, node):
-        return (node == self.node1 or node == self.node2)
-
+    return g
--- isomorphism.py
+++ isomorphism.py
@@ -1,1 +1,24 @@
-print "hi, Acsa"
+import ParsePy
+from igraph import *
+
+def initParse():
+    ParsePy.APPLICATION_ID = "WGl0x5YuJqhwg8dbYG16snlA2WG5X26JJrI7d"
+    ParsePy.MASTER_KEY = "9viWGl0x5YuJqhwg8dbYG16snlA2WG5X26JJrI7d"
+    
+def fetchGraph():
+    g = Graph()
+    query = ParsePy.ParseQuery("Node")
+    nodes = query.fetch()
+    
+    for node in nodes:
+        print("node (%.6f, %.6f)" % (node.lat, node.lng))
+
+
+
+
+
+
+
+
+
+    return g
